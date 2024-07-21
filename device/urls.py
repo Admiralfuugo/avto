f466aa8990ce7466bf419c4c371e921a2e36a629
muhiddinov <muhiddinovabulqosim@gmail.com>
2024-07-21 17:27:57 +0500

    Show data and device

--- device/urls.py
+++ device/urls.py
@@ -3,7 +3,7 @@ from .views import putHistory, getHistory, getDevices
 
 urlpatterns = [
     path('put/', putHistory, name='put-history'),
-    path('get/<pk>', getHistory, name='get-history'),
-    path('get/', getDevices, name='get-device'),
+    path('get/<pk>', getHistory, name='show-history'),
+    path('get/', getDevices, name='show-device'),
     
 ]
