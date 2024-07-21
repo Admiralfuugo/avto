2df6eb8fc488ca4e27de2e1ab89d275df8ba7824
muhiddinov <muhiddinovabulqosim@gmail.com>
2024-07-21 17:21:14 +0500

    Date time fields add

--- device/models.py
+++ device/models.py
@@ -4,6 +4,8 @@ class Device(models.Model):
     name = models.CharField(max_length=100)
     imei = models.CharField(max_length=20)
     location = models.CharField(max_length=50)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
     
     def __str__(self) -> str:
         return self.name
@@ -13,6 +15,7 @@ class History(models.Model):
     height = models.CharField(max_length=10)
     weight = models.CharField(max_length=30)
     temperature = models.CharField(max_length=10)
+    datetime = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
         return str.format('{} {}', self.height, self.weight)
